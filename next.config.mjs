4d6b0cafa0d94ff501b39e2421eb96f7da2af223
Pavan-Silva <119662370+Pavan-Silva@users.noreply.github.com>
2024-03-21 00:38:28 +0530

    disabled image optimization

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { basePath: "/Careers-Frontend", output: "export" };
+const nextConfig = {
+  basePath: "/Careers-Frontend",
+  output: "export",
+  images: { unoptimized: true },
+};
 
 export default nextConfig;
