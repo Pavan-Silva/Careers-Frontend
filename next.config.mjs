8bf602d2193c08c8779310f60592061d20846bd9
Pavan-Silva <119662370+Pavan-Silva@users.noreply.github.com>
2024-03-20 21:28:00 +0530

    build -added github pages support

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  /** Github Pages */
+  output: "export",
+  basePath: "/Careers-Frontend",
+};
 
 export default nextConfig;
