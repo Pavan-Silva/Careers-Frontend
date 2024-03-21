e270098e7ddae9341406da38f5f0147cf25b814e
Pavan-Silva <119662370+Pavan-Silva@users.noreply.github.com>
2024-03-21 07:56:26 +0530

    New github actions workflow setup

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+<<<<<<< HEAD
   basePath: "/Careers-Frontend",
   output: "export",
   images: { unoptimized: true },
+=======
+  output: "export",
+  distDir: "dist",
+>>>>>>> parent of 011a88b (removed all github pages configs)
 };
 
 export default nextConfig;
